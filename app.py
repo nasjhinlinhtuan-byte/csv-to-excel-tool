2032314c5831634e1392522f865a9b52cb185a57
nasjhinlinhtuan-byte <nasjhinlinhtuan@gmail.com>
2026-01-16 15:12:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ import pandas as pd
 from openpyxl import load_workbook
 import tempfile
 
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -36,3 +43,4 @@ def process():
 
 if __name__ == "__main__":
     app.run()
+
